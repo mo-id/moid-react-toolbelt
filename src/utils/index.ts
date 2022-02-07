7474a87c33bed89418cf64b4f278d9e207c3ab2c
Federico Baldini <federicobaldini@192.168.1.57>
2022-02-07 19:02:04 +0100

    remove useless utils functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
-export * from "./excludeFromChildren";
-export * from "./findChildByType";
+export * from "./generateId";
 export * from "./getDateFromISODate";
+export * from "./isProductionEnvironment";
+export * from "./isTestEnvironment";
+export * from "./noOp";
